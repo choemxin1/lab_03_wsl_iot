bc2c4fa72c6e737fd9466fd677b4131407ab3742
phugia1 <chotaotaodkm10@gmail.com>
2024-02-23 17:40:58 +0700

    first commit

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import time
 
 from Adafruit_IO import MQTTClient
 from simple_ai import *
+from uart import *
 AIO_FEED_IDs = ['nutnhan1','nutnhan2']
 AIO_USERNAME = "8192"
-AIO_KEY = "aio_yMhF31fpZ2tnQlMslr7KgqFPUFN9"
+AIO_KEY = "aio_QFvj35yUVcdmXakONes9BLhaHdie"
 
 def connected(client):
     print("Ket noi thanh cong ...")
@@ -38,24 +39,24 @@ sensor_type = 0
 pre =""
 ai_result=""
 while True:
-    counter = counter - 1
-    if counter <= 0:
-        counter = 10
-        if sensor_type == 0:
-            print('temp')
-            temp = random.randint(10,20)
-            client.publish('cambien1', temp)
-            sensor_type = 1
-        elif sensor_type == 1:
-            print('humi')
-            humi = random.randint(50, 70)
-            client.publish('cambien2', humi)
-            sensor_type = 2
-        elif sensor_type == 2:
-            print('light')
-            light = random.randint(100, 500)
-            client.publish('cambien3', light)
-            sensor_type = 0
+    # counter = counter - 1
+    # if counter <= 0:
+        # counter = 10
+        # if sensor_type == 0:
+        #     print('temp')
+        #     temp = random.randint(10,20)
+        #     client.publish('cambien1', temp)
+        #     sensor_type = 1
+        # elif sensor_type == 1:
+        #     print('humi')
+        #     humi = random.randint(50, 70)
+        #     client.publish('cambien2', humi)
+        #     sensor_type = 2
+        # elif sensor_type == 2:
+        #     print('light')
+        #     light = random.randint(100, 500)
+        #     client.publish('cambien3', light)
+        #     sensor_type = 0
     
     counter_ai = counter_ai -1
     if counter_ai <=0:
@@ -67,5 +68,6 @@ while True:
         if pre != ai_result:
             client.publish('ai', ai_result) 
     
+    readSerial(client)
     time.sleep(1)
     pass
