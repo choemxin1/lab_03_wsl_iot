b3feabeb8f0d613fd7faef1a445e1690400c6b5d
choemxin1 <160872427+choemxin1@users.noreply.github.com>
2024-03-09 17:40:58 +0700

    Update uart.py

--- uart.py
+++ uart.py
@@ -11,7 +11,8 @@ def getPort():
             splitPort = strPort.split(" ")
             commPort = (splitPort[0])
     #return commPort
-    return "COM3"
+    #return "COM3"
+    return "/dev/pts/9"
 
 if getPort()!= "None":
     ser = serial.Serial( port=getPort(), baudrate=115200)
